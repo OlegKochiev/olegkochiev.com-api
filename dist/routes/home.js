ede4f8641bede6a4da1061dc8eb9ff5a65d00ff9
Oleg Kochiev <developer-artisan@mail.ru>
2023-04-23 16:32:34 +0300

    fix

--- dist/routes/home.js
+++ dist/routes/home.js
@@ -1,11 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const constants_1 = require("../constants");
 const router = (0, express_1.Router)();
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+dotenv_1.default.config();
+mongoose_1.default.set('strictQuery', false);
+mongoose_1.default
+    .connect((_a = process.env.MONGO_CONNECTION_STRING) !== null && _a !== void 0 ? _a : '')
+    .then(() => {
+    console.log('Connected to MongoDB');
+})
+    .catch((error) => console.log(error));
 router.get(constants_1.PATHS.HOME(), (req, res) => {
     try {
-        return res.status(200).json(process.env);
+        return res.status(200).json(mongoose_1.default);
     }
     catch (error) {
         return res.status(500).send(error);
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,9 +3,22 @@ import {PATHS} from '../constants';
 
 const router = Router();
 
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+mongoose.set('strictQuery', false);
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING ?? '')
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => console.log(error));
+
 router.get(PATHS.HOME(), (req, res) => {
   try {
-    return res.status(200).json(process.env);
+    return res.status(200).json(mongoose);
   } catch (error) {
     return res.status(500).send(error);
   }
